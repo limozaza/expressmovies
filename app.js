2bcf7ca6f51124693c6baa978dcb27ccaf0f901c
limozaza <zakboite@gmail.com>
2017-07-14 11:38:31 +0200

    Add nodemon

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
-
 const app = express();
+const PORT = 3000;
 
 app.get('/',(req,res)=>{
     res.send("Bonjour Zakaria");
 });
 
-app.listen(3000,()=>{
+app.get('/admin',(req,res)=>{
+    res.send("Bonjour Zakaria admin");
+});
+
+app.listen(PORT,()=>{
     console.log('====================================');
     console.log('listen on port 3000');
     console.log('====================================');
