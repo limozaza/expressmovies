8e6c6c9c7188b2db0eea704ad1a25e8f292b8121
limozaza <zakboite@gmail.com>
2017-07-29 17:30:30 +0200

    Add Ejs Template

--- app.js
+++ app.js
@@ -2,32 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Bonjour Zakaria");
-});
+app.use('/public', express.static('public'));
 
+
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
 app.get('/movies',(req,res)=>{
     res.send("Bonjour movies");
 });
-
 app.get('/movies/add',(req,res)=>{
     res.send("Formulaire movies");
 });
-
 app.get('/movies/:id',(req,res)=>{
-
     const id = req.params.id;
     res.send(`Bonjour movie ${id}`);
 });
-
-
-
 app.get('/admin',(req,res)=>{
     res.send("Bonjour Zakaria admin");
 });
-
 app.listen(PORT,()=>{
-    console.log('====================================');
     console.log('listen on port 3000');
-    console.log('====================================');
 })
